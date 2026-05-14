f02ba718d16a521d62a9964f904b86b8a79a95a3
imetto <ahmed.haidar1993@gmail.com>
2026-05-14 10:21:03 +0200

    bb

--- Mogul/obj/Debug/net6.0/Mogul.AssemblyInfo.cs
+++ Mogul/obj/Debug/net6.0/Mogul.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mogul")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a730dba59e233f1eb52aa2ce9a2d3552bbe35367")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54134759467e6d5206dcb67368d796822861d88c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mogul")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mogul")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
